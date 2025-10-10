ccfcf71e5054d0360184a412e19bb7de0b9d2797
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-10 23:56:51 +0530

    added pair sum  problem in two pointers

---
+++ Arrays/sliding-window-and-two-pointers/pair-sum.js
@@ -0,0 +1,25 @@
+const pairSumOptimal = (nums, target) => {
+    const ans = [];
+    let start = 0;
+    let end = nums.length-1;
+    while(start < end) {
+        if(nums[start] + nums[end] === target) {
+            ans.push(start);
+            ans.push(end);
+            return ans;
+        } else if(nums[start] + nums[end] < target) {
+            start++;
+        } else {
+            end--;
+        }
+    }
+}
+
+function main() {
+  const nums = [2, 7, 1, 11, 13, 15];
+  const target = 24;
+  const ans = pairSumOptimal(nums, target);
+  console.log(ans); // [ 3, 4 ]
+}
+
+main();
