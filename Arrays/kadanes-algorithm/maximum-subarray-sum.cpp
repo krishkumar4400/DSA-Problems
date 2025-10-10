ff512a741b3a6f6535930a52954e9f16b7235653
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-10 22:33:23 +0530

    added maximum  subarray sum  problem in kadane's algorithm

--- Arrays/kadanes-algorithm/maximum-subarray-sum.cpp
+++ Arrays/kadanes-algorithm/maximum-subarray-sum.cpp
@@ -1,22 +1,54 @@
-#include<iostream>
-#include<vector>
+// 1 idea
 
-using namespace std;
+// Kadane finds the maximum sum of any contiguous subarray in O(n)
+// by keeping a running currentSum and restarting it whenever continuing would make things worse;
+// track the best(maxSum)
+// seen so far.
 
-void printSubArray(vector<int>nums) {
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+    using namespace std;
+
+// brute force approach
+void maximumSubArraySum(vector<int> nums)
+{ // O(n^3)
     int n = nums.size();
-    for(int start = 0; start < n; start++) {
-        for(int end = start; end < n; end++) {
-            for(int i = start; i <= end; i++) {
-                cout<<nums[i];
+    int maxSum = INT32_MIN;
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            int currSum = 0;
+            for (int i = start; i <= end; i++)
+            {
+                currSum += nums[i];
+                maxSum = max(maxSum, currSum);
             }
-            cout << " ";
         }
-        cout<<endl;
     }
+    cout << maxSum << endl; // 23
 }
 
-int main() {
-    vector<int>nums = {1,2,3,4,5};
-    printSubArray(nums);
+// optimal approach (using kadane's algorithm)
+void maximumSubArraySumOptimal(vector<int> nums)
+{
+    int n = nums.size();
+    int currentSum = 0, maxSum = INT32_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        currentSum += nums[i];
+        maxSum = max(maxSum, currentSum);
+        if(currentSum < 0) {
+            currentSum = 0;
+        }
+    }
+    cout << maxSum << endl; // 15
+}
+
+int main()
+{
+    vector<int> nums = {3, -4, 5, 4, -10, 7, 8};
+    maximumSubArraySumOptimal(nums); 
 }
