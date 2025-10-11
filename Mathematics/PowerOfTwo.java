395a911d5fe1dd74a619c856330b3f0f52e25f56
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-11 23:43:52 +0530

    added power of 2 problem  in mathematics

--- Mathematics/PowerOfTwo.java
+++ Mathematics/PowerOfTwo.java
@@ -16,14 +16,9 @@ public class PowerOfTwo {
         return n == 1;
     }
 
+    // bit manipulation
     public static boolean powerOfTwoOptimal(int n) {
-        if(n <= 0) {
-            return false;
-        }
 
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        return n == 1;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
