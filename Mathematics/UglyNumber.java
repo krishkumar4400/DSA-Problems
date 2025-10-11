e1aa05cf9db9cd974245ee6a4da2910e1dd2eb88
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-12 00:02:05 +0530

    added ugly number problem in mathematics

---
+++ Mathematics/UglyNumber.java
@@ -0,0 +1,28 @@
+public class UglyNumber {
+    public static void main(String[] args) {
+        int n = 15;
+
+        // factors -> 2,3,5
+
+        System.out.println(uglyNumber(n)); // 15
+    }
+
+    public static boolean uglyNumber(int n) {
+        if(n <= 0) {
+            return false;
+        }
+
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+
+        while (n % 5 == 0) {
+            n /= 5;
+        }
+        return n == 1;
+    }
+}
