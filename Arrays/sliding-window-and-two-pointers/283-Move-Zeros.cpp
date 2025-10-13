9170e63b452030d5809145d1df0f48afcd5f42fb
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-13 23:53:36 +0530

    Added move zeros  problem in two pointers

---
+++ Arrays/sliding-window-and-two-pointers/283-Move-Zeros.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int j = 0; // index for placing non-zero elements
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != 0)
+            {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
