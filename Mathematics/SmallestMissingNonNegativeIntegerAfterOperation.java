74f032275c852c44657f88ef0e29b0a3cf10db1d
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-17 22:27:03 +0530

    sm number

--- Mathematics/SmallestMissingNonNegativeIntegerAfterOperation.java
+++ Mathematics/SmallestMissingNonNegativeIntegerAfterOperation.java
@@ -1,3 +1,7 @@
-public class SmallestMissingNonNegativeIntegerAfterOperation-s {
-    
+import java.util.*;
+
+public class SmallestMissingNonNegativeIntegerAfterOperation {
+    public static void main(String[] args) {
+        Vector<Integer>nums = 
+    }
 }
