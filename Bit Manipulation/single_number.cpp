838b7a00e0c56735e2b43ac898af922d6c8f04fd
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-01-08 21:10:12 +0530

    Add XOR-based solution for Single Number

--- Bit Manipulation/single_number.cpp
+++ Bit Manipulation/single_number.cpp
@@ -1,7 +1,52 @@
 #include<iostream>
+#include<vector>
+#include<unordered_map>
+
 using namespace std;
 
-int main(){
+int singleNumberBrute(vector<int>&nums, int n) {
+    unordered_map<int, int> freq;
+    int ans=0;
+    for(int value:nums) {
+        freq[value]++;
+    }
     
+    for(auto it : freq) {
+        if(it.second == 1) {
+            return it.first;
+        }
+    }
+    return -1;
+}
+
+int singleNumberB(vector<int> &nums, int n) {
+    int single=0;
+    for(int i = 0; i<n; i++) {
+        int ans = nums[i];
+        for(int j = 0; j < n; j++) {
+            if(ans == nums[j] && !i==j) {
+                break;
+            }
+            single=ans;
+        }
+    }
+    return single;
+}
+
+int singleNumber(vector<int> &nums, int n) {
+    int ans = 0;
+    for(int i=0; i<n; i++) {
+        ans ^= nums[i];
+    }
+    return ans;
+}
+
+int main(){
+    vector<int>nums = {4,1,2,1,2};
+    int n = nums.size();
+    // singleNumberBrute(nums, n);
+    // cout<<singleNumber(nums, n) <<endl;
+    cout << singleNumberB(nums, n) << endl;
+
     return 0;
 }
